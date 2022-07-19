d0b5267d8d4092dd30b5ab142b11d2700204a7e9
RedWhite86 <martynov-f@yandex.ru>
2022-07-19 16:19:57 +0300

    first commit variables

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,13 +1,29 @@
+"use strict";
+
 function solveEquation(a, b, c) {
+  let root1;
+  let root2;
   let arr;
-  // код для задачи №1 писать здесь
-  return arr; // array
+  let d = b**2-4*a*c;
+  if (d>0){
+   root1 = -b+Math.sqrt(d)/(2*a); 
+   root2 = -b-Math.sqrt(d)/(2*a);
+   arr=[root1,root2];
+  }else if(d===0){
+   root1 = -b/(2*a); 
+   arr=[root1];
+  }else if(d<0){
+    root1 = null;
+    root2 = null;
+    arr = [];
+  }
+  return arr; 
 }
 
-function calculateTotalMortgage(percent, contribution, amount, date) {
-  let totalAmount;
+//function calculateTotalMortgage(percent, contribution, amount, date) {
+  //let totalAmount;
 
   // код для задачи №2 писать здесь
 
-  return totalAmount;
-}
+  //return totalAmount;
+//}
