e8eaffdfe338eb86f5b3a1a32d16953360dcd830
RedWhite86 <martynov-f@yandex.ru>
2022-07-26 15:43:29 +0300

    Все три задачи

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -17,23 +17,50 @@ function getArrayParams(arr) {
 
 // Задание 2
 function worker(arr) {
-  let sum;
-
-  // Ваш код
+  let sum = 0;
+  for (i=0; i<arr.length; i++){
+    sum += arr[i];
+  }
 
   return sum;
 }
 
-function makeWork(arrOfArr, func) {
-  let max;
+function makeWork(arrOfArr, worker) {
+  let max = null;
+  for (let i = 0; i < arrOfArr.length; i++){
+    if (max == null){
+      max = worker(arrOfArr[i])
+    }
+    if (max < worker(arrOfArr[i])){
+      max = worker(arrOfArr[i])
+    }
+  }
 
-  // Ваш кода
-  // for ...
-  
   return max;
 }
 
 // Задание 3
 function worker2(arr) {
-  // Ваш код
+  let max = null;
+    let min = null;
+    for (let i = 0; i < arr.length; i++) {
+      if(max == null) {
+        max = arr[i]
+      }
+      if (max < arr[i]) {
+        max = arr[i]
+      }
+    }
+    
+    for (let i = 0; i < arr.length; i++) {
+      if(min == null) {
+        min = arr[i]
+      }
+      if (min > arr[i]) {
+        min = arr[i]
+      }
+    }
+  
+    return Math.abs(max - min);
+
 }
