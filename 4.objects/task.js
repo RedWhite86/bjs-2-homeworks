6a5a3c662d04706b7a593b0755a5d53513f32dc7
RedWhite86 <martynov-f@yandex.ru>
2022-08-06 19:08:03 +0300

    Task 4 Objects

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -31,7 +31,7 @@ Student.prototype.getAverage = function () {
 }
 
 Student.prototype.exclude = function (reason) {
-  delete this.subject
-  delete this.marks
+  delete this.subject;
+  delete this.marks;
   this.excluded = reason;
 }
